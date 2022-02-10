5d59e3cbf817b71a832d7dcf3151286feb178e1f
a123648305 <a123648305@126.comcom>
2022-02-10 15:21:31 +0800

    code

--- model/redies.js
+++ model/redies.js
@@ -3,7 +3,7 @@
  * @Author: wujian
  * @Date: 2022-02-10 12:17:44
  * @LastEditors: wujian
- * @LastEditTime: 2022-02-10 14:37:47
+ * @LastEditTime: 2022-02-10 15:20:51
  */
 
 const { createClient } = require('redis')
@@ -13,16 +13,17 @@ const connectReadies = async () => {
   })
   client.on('error', (err) => console.log('Redis Client Error', err))
   await client.connect()
+  return client
 }
 
 const setRedis = async (key, value) => {
-  await connectReadies()
+  const client = await connectReadies()
   await client.set(key, value)
   await client.quit()
 }
 
 const getRedis = async (key) => {
-  await connectReadies()
+  const client = await connectReadies()
   const value = await client.get(key)
   await client.quit()
   return value
