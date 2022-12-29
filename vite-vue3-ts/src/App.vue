64710ab26ed94d2c7960850cef38c04b1dbece3a
a123648305 <a123648305@126.comcom>
2022-12-29 10:33:53 +0800

    增加一个 录制视频的插件

--- vite-vue3-ts/src/App.vue
+++ vite-vue3-ts/src/App.vue
@@ -3,31 +3,41 @@
 
 
 <template>
-  <h1>Hello App!</h1>
-  <p>
-    <router-link to="/index">Go to Home</router-link>
-    <router-link to="/about">Go to About</router-link>
-  </p>
-  <!-- route outlet -->
-  <!-- component matched by the route will render here -->
-  <h2>{{ test }}</h2>
-  <var-button type="primary" @click="test++">主要按钮</var-button>
-  <var-button type="primary" @click="exportPng">export</var-button>
-  <div class="list-wrap" ref="domtest">
-    <div v-for="item in 10" :key="item" class="list-item">{{ item }}</div>
+  <div>
+    <h1>Hello App!</h1>
+    <p>
+      <router-link to="/index">Go to Home</router-link>
+      <router-link to="/about">Go to About</router-link>
+    </p>
+    <!-- route outlet -->
+    <!-- component matched by the route will render here -->
+    <h2>{{ test }}</h2>
+    <var-button type="primary" @click="test++">主要按钮</var-button>
+    <var-button type="primary" @click="exportPng">export</var-button>
+    <var-button type="wraning" @click="postRecord">postRecord</var-button>
+    <var-button type="success" @click="replay">replay</var-button>
+    <div class="list-wrap" ref="domtest">
+      <div v-for="item in 10" :key="item" class="list-item">{{ item }}</div>
+    </div>
+    <div class="replay_wrap" ref="videoRef"></div>
+    <router-view></router-view>
   </div>
-
-  <router-view></router-view>
 </template>
 
 
 <script setup lang="ts">
+import * as rrweb from "rrweb";
+import rrwebPlayer from "rrweb-player";
+import "rrweb-player/dist/style.css";
 import { computed, onMounted, watchEffect } from "@vue/runtime-core";
 import domToImage, { Options } from "dom-to-image";
 import { ref, watch } from "vue";
+import { reactive } from "@vue/reactivity";
 const test = ref(0);
 const domtest = ref();
 const imgWrap = ref();
+const videoRef = ref();
+const events = reactive([]);
 
 watch(test, (val, old) => {
   console.log(val, "test", old);
@@ -39,6 +49,20 @@ watchEffect(() => {
 
 onMounted(() => {
   console.log(",pimt");
+  rrweb.record({
+    emit(event, isCheckout) {
+      // isCheckout is a flag to tell you the events has been checkout
+      if (isCheckout) {
+        events.push([]);
+      } else {
+        events.push(event);
+      }
+    },
+    recordCanvas: true, // 记录 canvas 内容
+    checkoutEveryNms: 10 * 1000, // 每10s重新制作快照
+    checkoutEveryNth: 200, // 每 200 个 event 重新制作快照
+    packFn: rrweb.pack, // 压缩
+  });
 });
 
 const exportPng = () => {
@@ -69,6 +93,28 @@ const exportPng = () => {
       link.remove();
     });
 };
+
+const postRecord = () => {
+  console.log("post", events);
+  const body = JSON.stringify({ events });
+  fetch("http://YOUR_BACKEND_API", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body,
+  });
+};
+
+const replay = () => {
+  new rrwebPlayer({
+    target: videoRef.value, // customizable root element
+    props: {
+      events,
+      // unpackFn: rrweb.unpack,
+    },
+  });
+};
 </script>
 
 
@@ -98,5 +144,17 @@ const exportPng = () => {
     height: 200px;
   }
 }
+.replay_wrap {
+  border: 1px solid red;
+  width: 500px;
+  position: absolute;
+  height: 500px;
+  right: 50px;
+  top: 200px;
+}
 </style>
+
+
+
+
 
